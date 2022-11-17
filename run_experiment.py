a037b86921af5b54b4a040fcc6a74f62b12234cd
Hussam Djadi <djadih@utexas.edu>
2022-11-17 10:49:15 -0600

    Including CPP model and some refactoring

--- run_experiment.py
+++ run_experiment.py
@@ -19,34 +19,26 @@ def adversarial_debiasing_query(subject, model_list):
     # runs through the process of gathering user input to select the model, select the features, 
     # and returning the output while logging the choices 
     query_log = Query()
-    
-    model_selected = False
-    while not model_selected:
-        model_choice = input("Which model should be used? plain [1] adversarially debaised [2]")
-        if model_choice == '1':
-            model = model_list[0]
-            query_log.set_model_name("Adversarial_debiasing_plain")
-            model_selected = True
-        elif model_choice == '2':
-            model = model_list[1]
-            query_log.set_model_name("Adversarial_debiasing_debiased")
-            model_selected = True
-        elif model_choice == '-1':
-            return False
-        else:
-            print("Invalid input. ", end="")
 
-    sex_input = input("Indicate the person's gender: [m] or [f]\n")
+    model_names = ["Plain", "Adversarial_Debiased", "Calibrated_Eq_Odds_Postprocessing"]
+    
+    model_choice = input("Which model should be used? [0: Plain, 1: Adversarial, 2: Calibrated Odds]: ")
+    if model_choice == '-1':
+        return False
+    model = model_list[int(model_choice)]
+    model_choice = model_names[int(model_choice)]
+
+    sex_input = input("Indicate the person's gender: [m] or [f]: ")
     if sex_input == 'f':
         sex_input = 0.0
     elif sex_input == 'm':
         sex_input = 1.0
-    race_input = input("Indicate the person's race:\n")
+    race_input = input("Indicate the person's race: ")
     if race_input == "white":
         race_input = 0.0
     else:
         race_input = 1.0
-    age_input = int(input("Indicate the person's age in years:\n"))
+    age_input = int(input("Indicate the person's age in years: "))
     education_input = int(input("Indicate the person's number of years in education: [typically 0-13]:\n"))
 
     query_input = create_single_entry_adult_dataset(race_input, sex_input, age_input, education_input)
@@ -60,7 +52,6 @@ def adversarial_debiasing_query(subject, model_list):
     return True
 
 def run_experiment():
-
     # Implemented multi-threading so that the user can query the model while the model is training.
     with concurrent.futures.ThreadPoolExecutor() as executor:
         subject_info_t = executor.submit(get_subject_info)
